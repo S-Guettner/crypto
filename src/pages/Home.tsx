83e4fd30d660a9eb0bbc639c298c27fa72b9d7bd
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-25 20:07:34 +0100

    fetch with axios in express

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,7 @@ const Home = () => {
     const [cryptoData,setCryptoData] = useState([])
 
     useEffect(() => {
-        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
-        `)
+        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
---
+++ src/server/app.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/historical', {
+      headers: {
+        'X-CMC_PRO_API_KEY': 'ae4891ed-276a-46a2-8db8-29ee2575fabc',
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
